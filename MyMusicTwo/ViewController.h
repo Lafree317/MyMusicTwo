890e69ea226eb4af35bc48298489ca4ac55ef880
huchunyuan <huchunyuan@huchunyuandeMacBook-Pro.local>
2015-09-23 17:10:40 +0800

    Initial Commit

---
+++ MyMusicTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMusicTwo
+//
+//  Created by huchunyuan on 15/9/23.
+//  Copyright (c) 2015年 Lafree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
